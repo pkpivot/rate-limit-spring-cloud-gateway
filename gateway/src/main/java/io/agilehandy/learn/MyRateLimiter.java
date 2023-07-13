fbf753a08b6ad59ddaa02783c119d3886f737b0e
Paul Kelly <kellypa@vmware.com>
2023-07-13 18:57:58 +0100

    Add logging to rate limiter

--- gateway/src/main/java/io/agilehandy/learn/MyRateLimiter.java
+++ gateway/src/main/java/io/agilehandy/learn/MyRateLimiter.java
@@ -1,5 +1,7 @@
 package io.agilehandy.learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.context.annotation.Primary;
 import reactor.core.publisher.Mono;
@@ -8,9 +10,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
 public class MyRateLimiter implements RateLimiter  {
+
+    private final Logger logger = LoggerFactory.getLogger(MyKeyResolver.class);
+
     @Override
     public Mono<Response> isAllowed(String routeId, String id) {
+        logger.info("Limit request for {} with key {}", routeId, id);
         Response r = new Response(true, new HashMap<>());
         return Mono.just(r);
 
